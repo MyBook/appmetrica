115b20a0b565587f3d9be83fa26b1ead0c4d6717
Ptitsyn Andrey <andrey.ptitsyn86@gmail.com>
2017-12-14 18:02:33 +0300

    add tests

--- appmetrica/push/api.py
+++ appmetrica/push/api.py
@@ -2,10 +2,8 @@
 import json
 import logging
 import requests
+import datetime
 from appmetrica.push import exceptions
-
-# APPMETRICA_APP_ID = '416934'
-# APPMETRICA_PUSH_ACCESS_TOKEN = 'AQAAAAAiYljrAAS1-wqZtJm9NEN8k8_I3ZxJBWo'
 
 logger = logging.getLogger(__name__)
 
@@ -43,9 +41,6 @@ class API(object):
         }
         try:
             response_data = self._request('post', 'management/groups', json=data)
-        except exceptions.AppMetricaRequestError as exc:
-            logger.error('create_group request with json %s failed due to %s', data, exc, exc_info=True)
-            raise exceptions.AppMetricaCreateGroupError
         except Exception as exc:
             logger.error('create_group request with json %s failed due to %s', data, exc, exc_info=True)
             raise exceptions.AppMetricaCreateGroupError
@@ -60,113 +55,61 @@ class API(object):
         # }
         return response_data['group']['id']
 
-    def send_push(self, messages, devices, group_id, tag=None):
-        assert group_id
+    def send_push(self, group_id, devices=None, ios_message=None, android_message=None, tag=None):
+        """
+        Sends push messages to the specified devices
+        :param group_id: group to combine the sending in the report
+        :param devices: list of token objects like:
+            [
+                {
+                    "id_type": "appmetrica_device_id",
+                    "id_values": ["123456789", "42"]
+                },
+                {
+                    "id_type": "ios_ifa",
+                    "id_values": ["8A690667-6204-4A6A-9B38-85DE016....."]
+                },
+                {
+                    "id_type": "android_push_token",
+                    "id_values": ["eFfxdO7uCMw:APA91bF1tN3X3BAbiJXsQhk-..."]
+                }
+            ]
+        :param ios_message: push message for ios devices. dict like:
+            {
+                "silent": false,
+                "content": {
+                    "title": "string",
+                    "text": "string",
+                    "sound": "disable",
+                    "data": "string"
+                }
+            }
+        :param android_message: similarly ios_message param
+        :param tag: send tag to combine the sending in the report
+        :return: Identifier of the sending push (transfer_id)
+        """
+        if not devices:
+            logger.error('send push error: devices are not provided')
+            raise exceptions.AppMetricaSendPushError('devices are not provided')
+        if not ios_message and not android_message:
+            logger.error('send push error: messages are not provided')
+            raise exceptions.AppMetricaSendPushError('messages are not provided')
 
-        # A message that is processed by the application in the background without displaying to the user.
-        # Using silent push, you can send additional data, or check the delivery of notifications.
-        silent = False
+        messages = {}
+        if ios_message:
+            messages['iOS'] = ios_message
+        if android_message:
+            messages['android'] = android_message
 
-        content = {
-            'title': u'Заголовок',  # required for not-silent
-            'text': u'Содержимое',
-            'data': json.dumps({'book': 225473})
-        }
-
+        tag = tag or datetime.datetime.now().isoformat()  # default tag
         data = {
             'push_request': {
                 'group_id': group_id,
-                'tag': tag or 'test',
-                'messages': {
-                    # 'android': {
-                    #     'silent': silent,
-                    #     'content': content
-                    # },
-                    'iOS': {
-                        'silent': silent,
-                        'content': content
-                    }
-                },
-                'devices': [
-                    {
-                        'id_type': 'ios_ifa',
-                        'id_values': ['BABDF74E-92AC-45DD-9E3A-9FC6A6C6FCCA']
-                    }
-                    # {
-                    #     'id_type': 'android_push_token',
-                    #     'id_values': ['']
-                    # }
-                ]
+                'tag': tag,
+                'messages': messages,
+                'devices': devices,
             }
         }
-
-        # data = {
-        #     'push_request': {
-        #         'group_id': group_id,
-        #         'tag': tag or 'test',
-        #         'messages': {
-        #             'android': {
-        #                 'silent': silent,
-        #                 'content': {
-        #                     "title": "string",
-        #                     "text": "string",
-        #                     "icon": "string",
-        #                     "icon_background": -7829368,
-        #                     "image": "https://example.com/picture.jpg",
-        #                     "banner": "https://example.com/picture.jpg",
-        #                     "data": json.dumps(extra_data),
-        #                     "priority": -2,
-        #                     "collapse_key": 2001,
-        #                     "vibration": [0, 500],
-        #                     "led_color": 10000,
-        #                     "led_interval": 50,
-        #                     "led_pause_interval": 50,
-        #                     "time_to_live": 180
-        #                 },
-        #                 "open_action": {
-        #                     "deeplink": "yandexmaps://?"
-        #                 }
-        #             },
-        #             'iOS': {
-        #                 "silent": silent,
-        #                 "content": {
-        #                     "title": "string",
-        #                     "text": "string",
-        #                     "badge": 1,
-        #                     "sound": "disable",
-        #                     "media_attachment": "string",
-        #                     "expiration": 3600,
-        #                     "data": json.dumps(extra_data)
-        #                 },
-        #                 "open_action": {
-        #                     "url": "https://ya.ru"
-        #                 }
-        #             }
-        #         },
-        #         "devices": [
-        #             {
-        #                 "id_type": "appmetrica_device_id",
-        #                 "id_values": ["123456789", "42"]
-        #             },
-        #             {
-        #                 "id_type": "ios_ifa",
-        #                 "id_values": ["8A690667-6204-4A6A-9B38-85DE016....."]
-        #             },
-        #             {
-        #                 "id_type": "google_aid",
-        #                 "id_values": ["eb5f3ec8-2e3e-492f-b15b-d21860b....."]
-        #             },
-        #             {
-        #                 "id_type": "android_push_token",
-        #                 "id_values": ["eFfxdO7uCMw:APA91bF1tN3X3BAbiJXsQhk-..."]
-        #             },
-        #             {
-        #                 "id_type": "ios_push_token",
-        #                 "id_values": ["F6A79E9F844A24C5FBED5C58A4C71561C180F........."]
-        #             }
-        #         ]
-        #     }
-        # }
         try:
             response_data = self._request('post', 'send', json=data)
         except Exception as exc:
@@ -181,6 +124,17 @@ class API(object):
         return response_data['push_response']['transfer_id']
 
     def check_status(self, transfer_id):
+        """
+        Check status of sending pushes
+        raise AppMetricaCheckStatusError if got an error
+
+        :param transfer_id: Push identifier (return after call `send_push`)
+        :return: status of push:
+            failed — validation error. See `errors` field for detail
+            in_progress — validation success. Sending messages in process
+            pending — the request is accepted and waits for validation
+            sent — sending completed
+        """
         endpoint = 'status/{transfer_id}'.format(transfer_id=transfer_id)
 
         try:
@@ -206,7 +160,7 @@ class API(object):
             if errors:
                 raise exceptions.AppMetricaCheckStatusError(errors[0])
 
-        return response_data['transfer']['id'], response_data['transfer']['status']
+        return response_data['transfer']['status']
 
     def _request(self, method, endpoint, params=None, headers=None, json=None):
         params = params or {}
@@ -227,7 +181,6 @@ class API(object):
                          extra={'data': {'json': json, 'params': params}})
             raise exceptions.AppMetricaRequestError
 
-        print(response.content)
         try:
             response_data = response.json()
         except Exception as exc:
@@ -236,3 +189,30 @@ class API(object):
             raise exceptions.AppMetricaRequestError
 
         return response_data
+
+    @staticmethod
+    def build_ios_message(**kwargs):
+        """
+        Create message object for ios platform
+        :param kwargs:
+            title (required) - subject of push
+            text (required) - body of push
+            data (optional) - extra data for client
+        :return: Message object (dict)
+        """
+        # A message that is processed by the application in the background without displaying to the user.
+        # Using silent push, you can send additional data, or check the delivery of notifications.
+        silent = kwargs.get('silent') or False
+
+        content = {
+            'title': kwargs['title'],
+            'text': kwargs['text'],
+            'sound': kwargs.get('sound') or 'disable',
+        }
+        if 'extra_data' in kwargs:
+            content['data'] = json.dumps(kwargs['extra_data'])
+
+        return {
+            "silent": silent,
+            "content": content,
+        }
